7a2b57668c882695d8f63805418140c7eacc83d4
john1king <uifantasy@gmail.com>
2015-03-16 10:38:55 +0800

    标签列表样式

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     get :readme, on: :member
   end
 
-  resources :tags, only: [:new, :create, :update, :destroy]
+  resources :tags
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
