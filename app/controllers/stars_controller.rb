8e4f5480011a6abec83812a3ae8c0fdb7cb69742
john1king <uifantasy@gmail.com>
2015-03-17 22:03:35 +0800

    通过 stars 渲染模板

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,9 +2,10 @@ class StarsController < ApplicationController
   before_action :require_signed_in_user
 
   def index
-    @tags = @user.tags
     respond_to do |format|
-      format.html
+      format.html {
+        @tags = @user.tags
+      }
       format.js {
         load_stars
       }
@@ -16,16 +17,20 @@ class StarsController < ApplicationController
   def load_stars
     if params[:tag_id]
       @tag = @user.tags.find(params[:tag_id])
-      stars = @tag.stars
+      @stars = @tag.stars
       total = @tag.stars_count
     else
-      stars = @user.stars
+      @stars = @user.stars
       total = @user.stars_count
     end
-    stars =  stars.offset((page_params[:page] - 1)*page_params[:per]).limit(page_params[:per])
-    @repos = Repo.includes(stars: :tags).find(stars.pluck(:repo_id))
+    @stars =  @stars.offset((page_params[:page] - 1)*page_params[:per]).limit(page_params[:per])
+    @stars = @stars.includes(:repo, :tags)
      if (@page = page_params[:page]) * page_params[:per] < total
-      @next_page_url = repos_url(page_params.dup.tap {|p| p[:page] += 1})
+      next_page_params = page_params.dup.tap do |qs|
+        qs[:page] += 1
+        qs[:format] = :js
+      end
+      @next_page_url = stars_url(next_page_params)
     end
     render 'index.js'
   end
