b5380dd179eef1e8406176eed8554d752bee04b9
john1king <uifantasy@gmail.com>
2015-03-17 21:48:32 +0800

    加载 repo 接口转移到 StarsController

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -3,6 +3,40 @@ class StarsController < ApplicationController
 
   def index
     @tags = @user.tags
+    respond_to do |format|
+      format.html
+      format.js {
+        load_stars
+      }
+    end
+  end
+
+  private
+
+  def load_stars
+    if params[:tag_id]
+      @tag = @user.tags.find(params[:tag_id])
+      stars = @tag.stars
+      total = @tag.stars_count
+    else
+      stars = @user.stars
+      total = @user.stars_count
+    end
+    stars =  stars.offset((page_params[:page] - 1)*page_params[:per]).limit(page_params[:per])
+    @repos = Repo.includes(stars: :tags).find(stars.pluck(:repo_id))
+     if (@page = page_params[:page]) * page_params[:per] < total
+      @next_page_url = repos_url(page_params.dup.tap {|p| p[:page] += 1})
+    end
+    render 'index.js'
+  end
+
+  def page_params
+    { page: fetch_int(:page, 1), per: [fetch_int(:per, 30), 100].min }
+  end
+
+  def fetch_int(key, default)
+    value = params[key].to_i
+    value.zero? ? default : value
   end
 
 end
