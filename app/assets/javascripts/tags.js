475ce9ac6cae3c853cf82b5f9436d21cb3a9383e
john1king <uifantasy@gmail.com>
2015-03-21 23:23:11 +0800

    * 拖曳到标签上时显示标签记数增加的效果，需完善

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -14,31 +14,61 @@
   });
 
   // drag star to add tag
+  var stop_drag = false;
+  var $target;
+  var $pre_target;
+  var dragged_tags = [];
+  var dragged_star_id;
+
   $('#stars').on('dragstart', 'li', function(e){
-    e.originalEvent.dataTransfer.setData('star-id', $(this).data('star-id'));
+    var orig_event = e.originalEvent;
+    dragged_star_id = $(this).data('star-id');
+    dragged_tags = $(this).find('.tag').map(function(){ return $(this).text() }).get();
   });
 
   $('#tags')
-    .on('dragover', 'li', function(e) {
-      e.preventDefault();
-      e.originalEvent.dataTransfer.dropEffect = 'move';
-    })
-    .on('dragenter', 'li', function(e) {
-      e.preventDefault();
-    })
-    .on('drop', 'li', function(e){
-      e.preventDefault();
-      var oe = e.originalEvent;
-      var star_id = oe.dataTransfer.getData('star-id');
-      var $target = $(oe.target);
+    .on('dragenter', 'li', function(e){
+      var orig_event = e.originalEvent;
+      $pre_target = $target;
+      $target = $(orig_event.target);
       if (!$target.is('li')){
         $target = $target.parents('li');
       }
+      var tag_name = $target.find('.tag').contents()[1].nodeValue;
+      stop_drag = $.inArray(tag_name, dragged_tags) > 0;
+      if (!stop_drag){
+        $target.addClass('dropable');
+        $target.find('.counter').text(+ $target.find('.counter').text() + 1);
+      }
+    })
+    .on('dragover', 'li', function(e) {
+      if (!stop_drag){
+        e.preventDefault();
+        e.originalEvent.dataTransfer.dropEffect = 'move';
+      }
+    })
+    .on('dragleave', 'li',function(e){
+      var $leave_target;
+      if ($pre_target){
+        $leave_target = $pre_target;
+        $pre_target = null;
+      } else if (!stop_drag) {
+        $leave_target = $target;
+        $target = null;
+      }
+      if ($leave_target) {
+        $leave_target.removeClass('dropable');
+        $leave_target.find('.counter').text(+ $leave_target.find('.counter').text() - 1);
+      }
+    })
+    .on('drop', 'li', function(e){
+      var orig_event = e.originalEvent;
       var tag_id = $target.attr('id').split('-')[1];
       $.ajax({
         method: 'POST',
-        url: '/tags/' + tag_id + '/add_star?star_id=' + star_id,
+        url: '/tags/' + tag_id + '/add_star?star_id=' + dragged_star_id,
         dataType: 'script'
       });
+      $target = null;
     });
 });
