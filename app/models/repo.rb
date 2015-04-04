99809345b21d07fff77350cff88433a34056a992
john1king <uifantasy@gmail.com>
2015-04-04 16:20:07 +0800

    在 Star 模型上设置索引，以便于查询标签和用户

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,9 +1,4 @@
-require 'elasticsearch/model'
-
 class Repo < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-
   has_many :stars, -> { where(active: true).order(starred_at: :desc) }
   has_many :users, through: :stars
   has_one :readme
@@ -18,29 +13,6 @@ class Repo < ActiveRecord::Base
     created_at
     updated_at
   )
-
-  def as_indexed_json(options={})
-    as_json(
-      only: [:full_name, :description]
-    )
-  end
-
-  def self.search(query, options = {})
-    options[:page] ||= 1
-    options[:per_page] ||= 30
-    __elasticsearch__.search(
-      {
-        query: {
-          multi_match: {
-            query: query,
-            fields: ['full_name^10', 'description']
-          }
-        },
-        size: options[:per_page],
-        from: (options[:page] - 1) * options[:per_page]
-      }
-    )
-  end
 
   def self.create_from_github(github_repo)
     find_or_create_by!(id: github_repo[:id]) do |repo|
--- app/models/star.rb
+++ app/models/star.rb
@@ -1,6 +1,72 @@
 class Star < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :user, counter_cache: true
   belongs_to :repo, counter_cache: :stargazers_count
   has_many :stars_tags
   has_many :tags, through: :stars_tags
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :description, type: :string
+      indexes :repo do
+        indexes :full_name, type: :string
+        indexes :description, type: :string
+      end
+      indexes :user do
+        indexes :id, type: :integer
+      end
+      indexes :tags do
+        indexes :name, type: :string, index: :not_analyzed
+      end
+    end
+  end
+
+  def as_indexed_json(options={})
+    as_json(
+      only: [:description],
+      include: {
+        repo: {
+          only: [:id, :full_name, :description]
+        },
+        user: {
+          only: [:id]
+        },
+        tags: {
+          only: [:id, :name]
+        }
+      }
+    )
+  end
+
+  def self.search(query, user_id, options = {})
+    options[:page] ||= 1
+    options[:per_page] ||= 30
+    query_opts = {
+      multi_match: {
+        query: query,
+        fields: ['repo.full_name^10', 'repo.description']
+      }
+    }
+    term_opts = {
+      'user.id' => user_id
+    }
+    term_opts['tags.name'] = options[:tag] if options[:tag]
+    __elasticsearch__.search(
+      {
+        query: {
+          filtered: {
+            query: query_opts,
+            filter: {
+              term: term_opts
+            }
+          }
+        },
+        size: options[:per_page],
+        from: (options[:page] - 1) * options[:per_page]
+      }
+    )
+  end
+
 end
